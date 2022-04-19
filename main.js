b5eaec84ef465bdf7c28cc2ed5ddf4869906e3ca
brunofelipehp <brunofelipehp8@gmail.com>
2022-04-19 17:53:53 -0300

    Utilizando o metodo POST com o axios

---
+++ main.js
@@ -0,0 +1,28 @@
+
+const url = "http://localhost:5501/api"
+
+function getUsers() {
+    axios.get(url)
+    .then(response => {
+        apiResult.textContent = JSON.stringify(response.data)
+    })
+    .catch(error => console.error(error))
+}
+
+function addNewUser(newUser){
+    axios.post(url, newUser)
+    .then(response => {
+        console.log(response);
+    })
+    .catch(error => console.error(error))
+}
+
+const newUser = {
+    name: "Olivia Zars",
+    avatar: "https://picsum.photos/200/300",
+    city: "Rio Janeiro"
+}
+
+addNewUser(newUser)
+
+getUsers()
